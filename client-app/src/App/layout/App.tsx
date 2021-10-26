ad2fdf8da08ba28eafa840b5032a62be134686d3
Amber Volkmann <amber.volkmann@brenkbrothers.com>
2021-10-26 14:11:27 -0500

    added editing functionality

--- client-app/src/App/layout/App.tsx
+++ client-app/src/App/layout/App.tsx
@@ -8,8 +8,13 @@ import './styles.css';
 
 
 function App() {
+  // Below is the state(s) that we're using throughout our application
   // First arg is the name of the variable, is a function that we can use to set the state
   const [activities, setActivities] = useState<Activity[]>([]);
+  // Here we're creating a union of Activity and undefined, because the state will be undefined on page load since nothing is selected
+  const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
+
+  const [editMode, setEditMode] = useState(false);
 
   // Axios is promise based, so we'll have to do something after recieving the response
   useEffect(() => {
@@ -19,11 +24,46 @@ function App() {
     })
   }, [])
 
+  function handleSelectActivity(id: string) {
+    // This is going to loop of the activities array and search for an activity with a matching ID and return true.
+    setSelectedActivity(activities.find(x => x.id === id));
+  }
+
+  function handleCancelSelectActivity() {
+    setSelectedActivity(undefined);
+  }
+
+  function handleFormOpen(id?: string) {
+    id ? handleSelectActivity(id) : handleCancelSelectActivity();
+    setEditMode(true);
+  }
+
+  function handleFormClose() {
+    setEditMode(false);
+  }
+
+  function handleCreateOrEditActivity(activity: Activity) {
+    // Check for the presence of an activity ID, if we do that means that we're going to be editing the activity with setActvities
+    activity.id ? setActivities([...activities.filter(x => x.id !== activity.id), activity]) : setActivities ([...activities, activity]);
+    setEditMode(false);
+    // we're going to set the selected/displayed activity to the one we just created
+    setSelectedActivity(activity);
+  }
+
   return (
     <>
-      <NavBar />
+      <NavBar openForm={handleFormOpen}/>
         <Container style={{marginTop: '7em'}}>
-          <ActivityDashboard activities={activities}/>
+          <ActivityDashboard 
+            activities={activities} 
+            selectedActivity={selectedActivity}
+            selectActivity={handleSelectActivity}
+            cancelSelectActivity={handleCancelSelectActivity}
+            editMode={editMode}
+            openForm={handleFormOpen}
+            closeForm={handleFormClose}
+            createOrEdit={handleCreateOrEditActivity}  
+          />
       </Container>
     </>
   );
